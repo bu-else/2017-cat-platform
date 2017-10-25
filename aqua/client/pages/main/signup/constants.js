8e0bb930888f3506c01568ffbde4dfe940e044cf
Rebecca Jellinek <rebeccaxx@dhcp-wifi-8021x-155-41-61-224.bu.edu>
2017-10-25 14:52:43 -0400

    remove aqu

--- aqua/client/pages/main/signup/constants.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const FluxConstant = require('flux-constant');
-
-
-module.exports = FluxConstant.set([
-    'REGISTER',
-    'REGISTER_RESPONSE'
-]);
--- aqua/gulp/clean.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const Gulp = require('gulp');
-const Del = require('del');
-
-
-Gulp.task('clean', (cb) => {
-
-    Del('./public', cb);
-});
